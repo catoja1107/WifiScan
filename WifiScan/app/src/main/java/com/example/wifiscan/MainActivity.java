34d9e7184c7274740b924c477dc24eb84dae65cf
Ubbie <ubbiehawk@gmail.com>
2023-10-15 18:55:32 -0400

    Revert "Merge pull request #36"
    This reverts commit 2df5c0e73224e13b1369964faa0dc7b6f5778ac5, reversing
    changes made to ec2dac30d24a82d27bd94c86e2a28616f1627b50.

--- WifiScan/app/src/main/java/com/example/wifiscan/MainActivity.java
+++ WifiScan/app/src/main/java/com/example/wifiscan/MainActivity.java
@@ -1,64 +1,14 @@
 package com.example.wifiscan;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-
-        setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.toString()) {
-                    case "Home":
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_layout, HomeFragment.class, null)
-                                .setReorderingAllowed(true)
-                                .addToBackStack("home")
-                                .commit();
-                        break;
-                    case "Settings":
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_layout, SettingsFragment.class, null)
-                                .setReorderingAllowed(true)
-                                .addToBackStack("settings")
-                                .commit();
-                        break;
-                    default:
-                        return false;
-                }
-
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return true;
-            }
-        });
-
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_layout, HomeFragment.class, null)
-                .commit();
     }
 }
