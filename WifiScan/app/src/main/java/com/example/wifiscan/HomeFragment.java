71be9134275af6f26d1031f86fb8abd3f0ef1098
catoja1107 <jcato195@gmail.com>
2023-11-28 17:04:07 -0500

    Revert "Corrected Maps API feature and view"

--- WifiScan/app/src/main/java/com/example/wifiscan/HomeFragment.java
+++ WifiScan/app/src/main/java/com/example/wifiscan/HomeFragment.java
@@ -1,38 +1,17 @@
 package com.example.wifiscan;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.net.wifi.ScanResult;
 import android.os.Bundle;
 
-import androidx.core.app.ActivityCompat;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.MapView;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 
 import java.util.HashMap;
 import java.util.List;
@@ -42,7 +21,7 @@ import java.util.List;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment implements OnMapReadyCallback {
+public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,11 +31,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private GoogleMap mMap;
-    private MapView mMapView;
-
-    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -82,7 +56,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -92,19 +65,14 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        mMapView = (MapView) view.findViewById(R.id.mapView);
-        mMapView.onCreate(savedInstanceState);
-        mMapView.getMapAsync(this);
-        mMapView.onResume();
         view.findViewById(R.id.scanOnceButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 WifiScanner wifiScanner = new WifiScanner(getActivity(), new WifiScannerListener() {
                     @Override
                     public void onWifiScanResult(List<ScanResult> scanResults) {
-                        if (scanResults.size() == 0) {
+                        if(scanResults.size() == 0) {
                             return;
                         }
 
@@ -113,7 +81,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
 
                         int counter = 0;
-                        for (ScanResult scanResult : scanResults) {
+                        for(ScanResult scanResult : scanResults) {
                             HashMap<Object, Object> network = new HashMap<>();
                             network.put("ssid", scanResult.SSID);
                             network.put("bssid", scanResult.BSSID);
@@ -142,53 +110,5 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         });
 
         return view;
-
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Check and request location permission
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Request permissions here
-            return;
-        }
-
-        // Enable My Location layer on the map
-        mMap.setMyLocationEnabled(true);
-
-        // Initialize FusedLocationProviderClient
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
-
-        // Get the last known location
-        fusedLocationProviderClient.getLastLocation()
-                .addOnSuccessListener(requireActivity(), location -> {
-                    if (location != null) {
-                        // Get the current location and move the camera to that location
-                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-                    }
-                });
-
-        // Set up location updates
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(100000);
-
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult != null) {
-                    Location location = locationResult.getLastLocation();
-                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-                }
-            }
-        }, null);
     }
 }
