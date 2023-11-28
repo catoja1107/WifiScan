edf327e4c696ac265a97c72c72fca11e0a34b5ce
James Cato <jcato195@gmail.com>
2023-11-28 17:30:14 -0500

    Revert "Updated Settings Layout"
    This reverts commit 1f4eb5ebc0ba1a7806d14457f7eede935b91c5ec.

--- WifiScan/app/build.gradle.kts
+++ WifiScan/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.preference:preference-ktx:1.2.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -43,7 +43,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:15.0.1")
-    implementation("androidx.preference:preference:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
